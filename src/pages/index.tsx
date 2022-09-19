c048a8926864487db91517487c83c57a014bd79c
arche <zxk44944@qq.com>
2022-09-19 17:53:12 +0800

    what is the plan

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
 export default function HomePage() {
-  return <h3>Hold on, the "Count It" is coming...</h3>;
+  return (
+    <>
+      <h3>Hold on, the "Count It" is coming...</h3>
+      <section>
+        <h3>What is the plan?</h3>
+        <ul>
+          <li>
+            <p>Where is the code repo?</p>
+            <p>We put it in github</p>
+          </li>
+          <li>
+            <p>What framework do I use?</p>
+            <p>I use the umi of antd</p>
+          </li>
+          <li>
+            <p>Where is the site hosted?</p>
+            <p>I use vercel.com for hosting.</p>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
 }
