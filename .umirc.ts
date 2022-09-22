67bd5dfaf9289375723b49e2a88f9c4f635e66b0
arche <zxk44944@qq.com>
2022-09-22 11:37:24 +0800

    setup google analytics

--- .umirc.ts
+++ .umirc.ts
@@ -1,3 +1,12 @@
+const googleAnalyticsScripts = [
+  {
+    src: 'https://www.googletagmanager.com/gtag/js?id=G-P6MVK2ER51',
+    async: true,
+  },
+  '/ga-setup.js',
+];
+
 export default {
-  npmClient: 'pnpm'
+  npmClient: 'pnpm',
+  headScripts: [...googleAnalyticsScripts],
 };
